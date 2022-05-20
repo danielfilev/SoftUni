ca807266b7dcab48d1f45e4286324fc41111f38d
danielfilev <104256434+danielfilev@users.noreply.github.com>
2022-05-20 12:35:18 +0300

    added fourth ex

---
+++ Advanced/Lists as Stacks and Queues/Lab/04.water_dispenser.py
@@ -0,0 +1,31 @@
+from collections import deque
+
+queue = deque()
+water_quantity = int(input())
+
+while True:
+    command = input()
+    if command == "Start":
+        break
+
+    queue.append(command)
+
+while True:
+    command = input()
+    if command == "End":
+        break
+
+    elif command.startswith('refill '):
+        params = command.split(' ')
+        water_quantity += int(params[1])
+    else:
+        person = queue.popleft()
+        water_wanted = int(command)
+
+        if water_wanted <= water_quantity:
+            print(f"{person} got water")
+            water_quantity -= water_wanted
+        else:
+            print(f"{person} must wait")
+
+print(f"{water_quantity} liters left")
