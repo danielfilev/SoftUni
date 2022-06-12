91fa54057c016cff98de6825a9c70fdde96e81a8
danielfilev <104256434+danielfilev@users.noreply.github.com>
2022-06-12 19:42:09 +0300

    added thrid ex

---
+++ Advanced/Functions Advanced/Lab/03.cheese_showcase.py
@@ -0,0 +1,21 @@
+def sorting_cheeses(**kwargs):
+
+    sorted_kwargs = sorted(
+        kwargs.items(), key=lambda x: (-len(x[1]), x[0]),
+    )
+
+    result = []
+    for name, cheese in sorted_kwargs:
+        result.append(name)
+        result.extend(sorted(cheese, reverse=True))
+
+    return '\n'.join([str(x) for x in result])
+
+
+print(
+    sorting_cheeses(
+        Parmesan=[102, 120, 135],
+        Camembert=[100, 100, 105, 500, 430],
+        Mozzarella=[50, 125],
+    )
+)
